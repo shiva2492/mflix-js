99d47d52a974b80092719c05d6eb00504e87dbed
Shiva Dwivedi <shivadwivediit@gmail.com>
2020-02-16 12:28:02 +0400

    ticket connectionPooling and wTimeouts resolved

--- src/index.js
+++ src/index.js
@@ -9,23 +9,24 @@ const port = process.env.PORT || 8000
 /**
 Ticket: Connection Pooling
 
-Please change the configuration of the MongoClient object by setting the
+Changed the configuration of the MongoClient object by setting the
 maximum connection pool size to 50 active connections.
 */
 
 /**
 Ticket: Timeouts
 
-Please prevent the program from waiting indefinitely by setting the write
+Prevented the program from waiting indefinitely by setting the write
 concern timeout limit to 2500 milliseconds.
 */
 
 MongoClient.connect(
-  process.env.MFLIX_DB_URI,
-  // TODO: Connection Pooling
-  // Set the poolSize to 50 connections.
-  // TODO: Timeouts
+  process.env.MFLIX_DB_URI,{
+  // Sets the poolSize to 50 connections.
+  poolSize: 50,
   // Set the write timeout limit to 2500 milliseconds.
+  wtimeout: 2500
+  },
   { useNewUrlParser: true },
 )
   .catch(err => {
